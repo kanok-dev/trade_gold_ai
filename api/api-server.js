194b1e7b9de81c9bc3128d37196c5cf762820b15
kanok <kanok.santhong@gmail.com>
2025-07-07 17:19:31 +0700

    feat: Add new frontend URL to CORS configuration for enhanced accessibility

--- api/api-server.js
+++ api/api-server.js
@@ -93,7 +93,7 @@ class GoldTradingAPIServer {
     // CORS configuration
     this.app.use(
       cors({
-        origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:4173', 'https://trade-gold-ai.onrender.com'], // Frontend URLs
+        origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:4173', 'https://trade-gold-ai.onrender.com', 'golden-api.kanoks.me'], // Frontend URLs
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
